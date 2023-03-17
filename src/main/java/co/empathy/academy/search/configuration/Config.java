635dce2c3f31c751b1d93f2ca2ac49304ff1654c
juanma6245 <juanma6245@hotmail.com>
2023-03-17 12:56:26 +0100

    Jobrunr working

--- src/main/java/co/empathy/academy/search/configuration/Config.java
+++ src/main/java/co/empathy/academy/search/configuration/Config.java
@@ -6,6 +6,12 @@ package co.empathy.academy.search.configuration;
 import co.empathy.academy.search.repository.ElasticConnection;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.server.JobActivator;
+import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
@@ -26,5 +32,11 @@ public class Config implements AsyncConfigurer {
         return new ElasticConnection(RestClient.builder(new HttpHost(hostname, port)).build());
     }
 
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper) {
+        InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(jobMapper);
+        return storageProvider;
+    }
 
 }
