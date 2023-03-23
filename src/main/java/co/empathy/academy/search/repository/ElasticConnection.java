f36b9b4ad7dfc59968fb88d831a55c7e32e341f8
juanma6245 <juanma6245@hotmail.com>
2023-03-23 10:42:28 +0100

    Index methods added to ElacticConnection

--- src/main/java/co/empathy/academy/search/repository/ElasticConnection.java
+++ src/main/java/co/empathy/academy/search/repository/ElasticConnection.java
@@ -1,9 +1,13 @@
 package co.empathy.academy.search.repository;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.empathy.academy.search.common.TSVtype;
+import co.empathy.academy.search.model.title.Title;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
 import org.elasticsearch.client.RestClient;
@@ -34,5 +38,14 @@ public class ElasticConnection {
     public String getClusterName() throws IOException {
 
         return this.client.cluster().health().clusterName();
+    }
+
+    public IndexResponse index(Title document, TSVtype type) throws IOException {
+        IndexRequest<Title> request = IndexRequest.of(i -> i
+                .index(type.toString().toLowerCase())
+                .id(null)
+                .document(document)
+        );
+        return this.client.index(request);
     }
 }
