a323bb6df72457b1f2251a067f014df0e1582fb9
juanma6245 <juanma6245@hotmail.com>
2023-03-23 10:43:46 +0100

    Added index in elastic, and headers must be deleted from file before processing

--- src/main/java/co/empathy/academy/search/service/FileTaskServiceImpl.java
+++ src/main/java/co/empathy/academy/search/service/FileTaskServiceImpl.java
@@ -1,31 +1,35 @@
 package co.empathy.academy.search.service;
 
-import co.empathy.academy.search.common.CSVtype;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.empathy.academy.search.common.TSVtype;
 import co.empathy.academy.search.model.title.Title;
+import co.empathy.academy.search.repository.ElasticConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.List;
 
 @Service
 public class FileTaskServiceImpl implements FileTaskService{
 
-        @Autowired
-        private TSVService TSVService;
-        @Override
-        public void uploadFileTSV(File file, CSVtype type) throws InterruptedException, IOException {
-            //Handle file
-            //List<Title> titles = TSVService.readTSV(file, type);
-                long lines = Files.lines(file.toPath()).toArray().length - 1;
-                InputStream input = new FileInputStream(file);
-                BufferedReader br = new BufferedReader(new InputStreamReader(input));
-                String headers = br.readLine();
-                for (int i = 0; i < lines; i++) {
-                    Title title = TSVService.readOneTSV(br, type);
-                    //System.out.println(title);
-                    //Save in ElasticSearch
-                }
+    @Autowired
+    private TSVService TSVService;
+    @Autowired
+    private ElasticConnection elasticConnection;
+    @Override
+    public void uploadFileTSV(File file, TSVtype type) throws IOException {
+        //Handle file
+        //List<Title> titles = TSVService.readTSV(file, type);
+        long lines = Files.lines(file.toPath()).toArray().length;
+        InputStream input = new FileInputStream(file);
+        BufferedReader br = new BufferedReader(new InputStreamReader(input));
+        //String headers = br.readLine();
+        IndexResponse response;
+        for (int i = 0; i < lines; i++) {
+            Title title = TSVService.readOneTSV(br, type);
+            response = elasticConnection.index(title, type);
+            //System.out.println(response.id());
         }
+    }
 }
