222199b5c1cac7dd19048e057dfb65186930699c
juanma6245 <juanma6245@hotmail.com>
2023-04-11 11:13:02 +0200

    IndexControllerTest TODO

---
+++ src/test/java/co/empathy/academy/search/controller/IndexControllerTest.java
@@ -0,0 +1,18 @@
+package co.empathy.academy.search.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    @Test
+    void index() {
+        //TODO
+    }
+
+    @Test
+    void deleteIndex() {
+        //TODO
+    }
+}
