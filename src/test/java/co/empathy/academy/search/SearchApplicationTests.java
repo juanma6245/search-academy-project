49bcc2062cb94fff86eba385a0650d1a4bb10c67
juanma6245 <juanma6245@hotmail.com>
2023-04-12 09:40:58 +0200

    Bean creating error fixed

--- src/test/java/co/empathy/academy/search/SearchApplicationTests.java
+++ src/test/java/co/empathy/academy/search/SearchApplicationTests.java
@@ -1,9 +1,11 @@
 package co.empathy.academy.search;
 
+import co.empathy.academy.search.configuration.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(classes = TestConfig.class)
 class SearchApplicationTests {
 
 	@Test
